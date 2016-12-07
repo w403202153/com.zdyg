b9685e713bfe815d580ab608d07b37700c5dec2e
w403202153 <403202153@qq.com>
2016-12-07 13:26:06 +0800

    initial version

---
+++ ok/ok-api/src/main/java/com/amycat/utils/DateUtils.java
@@ -0,0 +1,8 @@
+package com.amycat.utils;
+
+public class DateUtils {
+	
+	public static void print(String word){
+		System.out.println(word);
+	}
+}
---
+++ ok/ok-server/src/main/java/com/amycat/utils/XXX.java
@@ -0,0 +1,5 @@
+package com.amycat.utils;
+
+public class XXX {
+
+}
